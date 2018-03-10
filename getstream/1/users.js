4e375a776a38d94b6112ca1f29bb4940e37f01db
Imam Assidiqqi <assidiqqi.imam@gmail.com>
2018-03-10 11:53:50 +0700

    Commit for stream

---
+++ getstream/1/users.js
@@ -0,0 +1,22 @@
+module.exports = [
+    {
+        name: 'Imam',
+        username: 'imam'
+    },
+    {
+        name: 'Adit',
+        username: 'adit'
+    },
+    {
+        name: 'Bunga',
+        username: 'bunga'
+    },
+    {
+        name: 'Toni',
+        username: 'toni'
+    },
+    {
+        name: 'Sandra',
+        username: 'sandra'
+    }
+]
